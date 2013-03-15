5a25935b27cf97f1df3a23b38791f01be44b682d
Brandon Krigbaum <brandon.krigbaum@cygnus.com>
2013-03-15 14:03:21 -0500

    Added Supermodlrversion trait

---
+++ classes/Trait/Supermodlrversions.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+  * FileDescription: Versions
+  */
+trait Trait_Supermodlrversions {
+    use Trait_Supertrait;
+    public static $__supermodlrversions__scfg = array(
+            'field_keys' => array(
+                )
+ );
+}
