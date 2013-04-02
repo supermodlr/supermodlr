1a5e7910af0397fa60fce3bf8611a29329a96c54
Brandon Krigbaum <brandon.krigbaum@cygnus.com>
2013-04-02 16:05:25 -0500

    Updated indentation and spacing for ModelId field

--- classes/Field/Supermodlrcore/ModelId.php
+++ classes/Field/Supermodlrcore/ModelId.php
@@ -1,21 +1,23 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-class Field_Supermodlrcore_ModelId extends Field  implements Interface_FieldStorage, Interface_FieldDataType
+
+class Field_Supermodlrcore_ModelId extends Field implements Interface_FieldStorage, Interface_FieldDataType
 {
-    use Trait_FieldStorage_Single, Trait_FieldDataType_String;
-    public $name = 'ModelId';
-    public $label = 'Model ID';
-    public $description = 'Model ID';
-    public $datatype = 'string';
-    public $charset = 'UTF-8';
-    public $storage = 'single';
-    public $required = TRUE;
-    public $unique = FALSE;
-    public $searchable = FALSE;
-    public $filterable = TRUE;
-    public $maxlength = 12;
-    public $nullvalue = FALSE;
-    public $hidden = FALSE;
-    public $private = FALSE;
-    public $readonly = FALSE;
+
+	use Trait_FieldStorage_Single, Trait_FieldDataType_String;
+	public $name = 'ModelId';
+	public $label = 'Model ID';
+	public $description = 'Model ID';
+	public $datatype = 'string';
+	public $charset = 'UTF-8';
+	public $storage = 'single';
+	public $required = FALSE;
+	public $unique = FALSE;
+	public $searchable = FALSE;
+	public $filterable = TRUE;
+	public $maxlength = 12;
+	public $nullvalue = FALSE;
+	public $hidden = TRUE;
+	public $private = FALSE;
+	public $readonly = FALSE;
 
 }
